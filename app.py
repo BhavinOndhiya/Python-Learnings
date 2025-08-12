7fdd43e129036fb59caedbce5b985c82e0e5cae9
Bhavin <Bhavinondhiya0@gmail.com>
2025-08-12 11:16:37 +0530

    Learning About Strings in Python

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 # print("Hello World !")
 
-print("    /|")
-print("   / |")
-print("  /  |")
-print(" /___|")
+# print("    /|")
+# print("   / |")
+# print("  /  |")
+# print(" /___|")
 
+# character_name = "Bhavin"
+# character_age = "35"
+#
+# print("There once was a man named" +character_name+", ")
+# print("he was " + character_age + " years old, ")
+# print("he really liked the name "+character_name+", ")
+# print("but didn't liked being " + character_age + ".")
+
+phrase = "Giraffe Academy"
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("A"))
+print(phrase.replace("Giraffe", "Elephant"))
